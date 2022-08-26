6995429bef76a04bd4c7d2f07694fe9599ccfd91
Tom-254 <211mseeratiego@gmail.com>
2022-08-26 00:11:15 +0000

    0x02. Python - import & modules

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -10,7 +10,8 @@ def magic_calculation(a, b):
         for i in range(4, 6):
             c = add(c, i)
         return c
-    return sub(a, b)
+    else:
+        return sub(a, b)
 
 
 print(dis(magic_calculation))
