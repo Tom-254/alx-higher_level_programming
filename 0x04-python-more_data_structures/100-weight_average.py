1d8034990183ba5dc59b803df549203f9709419d
Tom-254 <211mseeratiego@gmail.com>
2022-08-31 22:28:08 +0000

    0x01. Python - if/else, loops, functions

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    if my_list:
+        score_total = 0
+        weighted_sum = 0
+        for tup in my_list:
+            score_total += (tup[0] * tup[1])
+            weighted_sum += tup[1]
+        return (score_total / weighted_sum)
+    return 0
+
+if __name__ == "__main__":
+    my_list = [(1, 2), (2, 1), (3, 10), (4, 2)]
+    # = ((1 * 2) + (2 * 1) + (3 * 10) + (4 * 2)) / (2 + 1 + 10 + 2)
+    result = weight_average(my_list)
+    print("Average: {:0.2f}".format(result))
