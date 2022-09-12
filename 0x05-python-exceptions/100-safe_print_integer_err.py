a93e4b710b0da456fb7271d83e319ee126172baf
Tom-254 <211mseeratiego@gmail.com>
2022-09-12 21:31:15 +0000

    0x01. Python - if/else, loops, functions

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,7 +6,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except (ValueError) as e:
+    except (TypeError, ValueError) as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return False
 
