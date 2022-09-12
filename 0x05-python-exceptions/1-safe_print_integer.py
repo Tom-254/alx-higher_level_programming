d81be6da8b4c2f53c5f586d8def9bfce0efc4e8f
Tom-254 <211mseeratiego@gmail.com>
2022-09-12 22:00:38 +0000

    0x01. Python - if/else, loops, functions

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 
-from cmath import exp
-
-
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except (TypeError, ValueError):
+    except Exception as e:
         return False
 
 
