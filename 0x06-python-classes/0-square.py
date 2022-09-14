6570f45d0cdd1e7f9cda128c8b16e3cc6d539b3b
Tom-254 <211mseeratiego@gmail.com>
2022-09-14 22:04:00 +0000

    alx-python-project

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""Defines and Empty Square class"""
 
 class Square:
+    """Empty Square class"""
     pass
+
+
+if __name__ == "__main__":
+    my_square = Square()
+    print(type(my_square))
+    print(my_square.__dict__)
