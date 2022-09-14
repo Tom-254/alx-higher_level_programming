fc790e54d6c5a5ef2e7b8c25c3e162618bc0d87b
Tom-254 <211mseeratiego@gmail.com>
2022-09-14 23:00:03 +0000

    alx-python-project

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""Defines a class MagicClass"""
+import math
+
+
+class MagicClass:
+    """MagicClass Circle represention"""
+
+    def __init__(self, radius=0):
+        """Initilizes the MagicClass Instance
+            Args:
+                radius (int): the radius of a circle
+        """
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+
+        self.__radius = radius
+
+    def area(self):
+        """Calculates the area
+            Returns:
+                float: the area of the circle
+        """
+        return (self.__radius ** 2) * math.pi
+
+    def circumference(self):
+        """Calculates the circumference
+            Returns
+                float: the circumference of the circle
+        """
+        return 2 * math.pi * self.__radius
