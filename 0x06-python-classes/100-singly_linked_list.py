67e7343fd45ca9cea2333f7f1798d1f28194a698
Tom-254 <211mseeratiego@gmail.com>
2022-09-14 22:24:44 +0000

    alx-python-project

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -92,3 +92,19 @@ class SinglyLinkedList():
             print(tmp.data)
             tmp = tmp.next_node
         return (str(tmp.data))
+
+
+if __name__ == "__main__":
+    sll = SinglyLinkedList()
+    sll.sorted_insert(2)
+    sll.sorted_insert(5)
+    sll.sorted_insert(3)
+    sll.sorted_insert(10)
+    sll.sorted_insert(1)
+    sll.sorted_insert(-4)
+    sll.sorted_insert(-3)
+    sll.sorted_insert(4)
+    sll.sorted_insert(5)
+    sll.sorted_insert(12)
+    sll.sorted_insert(3)
+    print(sll)
