46badc844c1263d3c553bd4ceadb92cbe51c0b2b
Tom-254 <211mseeratiego@gmail.com>
2022-09-26 21:58:07 +0000

    0x0A. Python - Inheritance

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
 """
-Contains the class BaseGeometry
+Holds the class superclass BaseGeometry
 """
 
 
 class BaseGeometry:
-    """A class with public instance methods area and integer_validator"""
+    """BaseGeometry class containing area and integer_validator
+    """
+    def __init__(self):
+        """Initializes the class
+        """
+        pass
 
     def area(self):
-        """raises an exception when called"""
+        """Raises and exception when called
+        """
+
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """validates that value is an integer greater than 0"""
+        """Validates input passed to match conditions provided
+        """
+
         if type(value) is not int:
             raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
