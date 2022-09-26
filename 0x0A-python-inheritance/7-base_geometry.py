817544ef03ac19652c3ea6c9d631b6e5b4170532
Tom-254 <211mseeratiego@gmail.com>
2022-09-26 21:51:08 +0000

    0x0A. Python - Inheritance

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
-Module that holds the class BaseGeometry
+Holds the class superclass BaseGeometry
 """
 
 
-class BaseGeometry:
+class BaseGeometry(object):
     """BaseGeometry class containing area and integer_validator
     """
 
