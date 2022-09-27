a6024ab3393f0fbf4de12358c812bebca86a10bb
Tom-254 <211mseeratiego@gmail.com>
2022-09-27 22:48:08 +0000

    0x01. Python - if/else, loops, functions

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+""" that inserts a line of text to a file, after
+    each line containing a specific string
+"""
+
+
 def append_after(filename="", search_string="", new_string=""):
+    """ that inserts a line of text to a file, after
+        each line containing a specific string
+    """
     my_str = ""
     with open(filename, encoding="utf8") as f:
         for line in f:
