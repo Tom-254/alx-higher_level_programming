b85ae92798e99189a52b56e9f2f972bf543436e5
Tom-254 <211mseeratiego@gmail.com>
2022-09-27 21:45:37 +0000

    0x01. Python - if/else, loops, functions

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+"""function that creates an Object from a “JSON file”
+"""
 import json
 
 
 def load_from_json_file(filename=""):
+    """function that creates an Object from a “JSON file”
+    """
     with open(filename, encoding="utf-8") as fd:
         my_obj = json.load(fd)
 
