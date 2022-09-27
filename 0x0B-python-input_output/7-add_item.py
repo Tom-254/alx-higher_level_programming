089cf14c0199392484d755763fa031f1b55367ed
Tom-254 <211mseeratiego@gmail.com>
2022-09-27 21:55:55 +0000

    0x01. Python - if/else, loops, functions

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -18,6 +18,4 @@ def add_item(args, filename):
 
 
 if __name__ == "__main__":
-    args = sys.argv[1:]
-    filename = "add_item.json"
-    add_item(args, filename)
+    add_item(sys.argv[1:], "add_item.json")
