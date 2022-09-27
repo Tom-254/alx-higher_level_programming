5f51b364873eeb90680c1258bbe519cff5076688
Tom-254 <211mseeratiego@gmail.com>
2022-09-27 22:36:37 +0000

    0x01. Python - if/else, loops, functions

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
-"""Module 7-add_item.
-Adds all arguments to a Python list,
-and then save them to a file.
+""" adds all arguments to a Python
+    list, and then save them to a file:
 """
-
 import sys
-import json
-import os.path
+import os
+save_to_json = __import__("5-save_to_json_file").save_to_json_file
+load_from_json = __import__("6-load_from_json_file").load_from_json_file
 
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-my_file = 'add_item.json'
+def add_item(args, filename):
+    if (os.path.exists(filename)):
+        content = load_from_json(filename)
+    else:
+        content = []
+    content.extend(args)
+    save_to_json(content, filename)
 
-my_list = []
 
-if os.path.exists(my_file) and os.path.getsize(my_file) > 0:
-    my_list = load_from_json_file(my_file)
-
-if len(sys.argv) > 1:
-    for elem in sys.argv[1:]:
-        my_list.append(elem)
-
-save_to_json_file(my_list, my_file)
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    filename = "add_item.json"
+    add_item(args, filename)
