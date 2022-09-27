37f40671ba352b874f92c34e4089f3a8e553709e
Tom-254 <211mseeratiego@gmail.com>
2022-09-27 22:01:43 +0000

    0x01. Python - if/else, loops, functions

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-""" adds all arguments to a Python
-    list, and then save them to a file:
 """
-import sys
-import os
-save_to_json = __import__("5-save_to_json_file").save_to_json_file
-load_from_json = __import__("6-load_from_json_file").load_from_json_file
+Script that adds all arguments to a Python list, and then saves them to a file
+"""
 
-args = sys.argv[1:]
+from sys import argv
+save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
+
 filename = "add_item.json"
 
-if (os.path.exists(filename)):
-    content = load_from_json(filename)
-else:
-    content = []
-content.extend(args)
-save_to_json(content, filename)
+try:
+    json_list = load_from_json_file(filename)
+except:
+    json_list = []
+
+for arg in argv[1:]:
+    json_list.append(arg)
+
+save_to_json_file(json_list, filename)
