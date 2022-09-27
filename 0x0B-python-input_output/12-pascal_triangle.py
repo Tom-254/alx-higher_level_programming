588f34bdfc3169c8b781211845d15819cc1cdec0
Tom-254 <211mseeratiego@gmail.com>
2022-09-27 23:00:07 +0000

    0x01. Python - if/else, loops, functions

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,36 +1,22 @@
 #!/usr/bin/python3
-"""
-12-pascal_triangle.py
-Create a function def pascal_triangle(n): that returns
-a list of lists of integers representing the Pascal’s triangle of n
+"""returns a list of lists of integers
+    representing the Pascal’s triangle of n
 """
 
 
 def pascal_triangle(n):
+    """returns a list of lists of integers
+    representing the Pascal’s triangle of n
     """
-    returns pascal triangle of n.
-    """
-
     if n <= 0:
         return []
 
-    limit = n - 1
-    triangle = [[1]]
-
-    for i in range(limit):
+    p_list = []
+    for j in range(1, n + 1):
+        C = 1
         row = []
-        row.append(1)
-
-        if len(triangle[i]) > 1:
-            prev_row_len = len(triangle[i]) - 1
-            nxt = 1
-
-            for j in range(prev_row_len):
-                suma = triangle[i][j] + triangle[i][nxt]
-                row.append(suma)
-                nxt += 1
-
-        row.append(1)
-        triangle.append(row)
-
-    return triangle
+        for i in range(1, j + 1):
+            row.append(C)
+            C = C * (i - j) // j
+        p_list.append(row)
+    return p_list
