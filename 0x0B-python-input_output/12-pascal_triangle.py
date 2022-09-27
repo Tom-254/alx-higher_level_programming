b50899a38a768684acd23183abcf9389ff78592f
Tom-254 <211mseeratiego@gmail.com>
2022-09-27 23:02:25 +0000

    0x01. Python - if/else, loops, functions

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -12,11 +12,11 @@ def pascal_triangle(n):
         return []
 
     p_list = []
-    for j in range(1, n + 1):
+    for line in range(1, n + 1):
         C = 1
         row = []
-        for i in range(1, j + 1):
+        for i in range(1, line + 1):
             row.append(C)
-            C = C * (i - j) // j
+            C = (C * (line - i)) // i
         p_list.append(row)
     return p_list
