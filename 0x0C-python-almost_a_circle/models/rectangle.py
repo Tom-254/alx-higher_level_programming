baba578c0ff8ae5bc6d5e4b8e4b3dcf92bab6e30
Tom-254 <211mseeratiego@gmail.com>
2022-10-03 22:04:19 +0000

    0x0C. Python - Almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Module containing the rectangle class
 """
-from base import Base
+from models.base import Base
 
 class Rectangle(Base):
     """Class represention of a rectangle
