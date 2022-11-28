47e264980013641152de987ec2480dea2acd7efe
Tish254 <tiegomseeraoscar295@gmail.com>
2022-11-28 23:46:10 +0300

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-
-if (+process.argv[2]) {
-  console.log(`My number: ${+process.argv[2]}`);
-} else {
+const x = process.argv[2];
+if (isNaN(x) === true) {
   console.log('Not a number');
+} else {
+  console.log('My number: ' + x);
 }
