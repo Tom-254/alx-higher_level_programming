77569ff7581e339907852a364cfa5f7a9e8eb945
Tish254 <tiegomseeraoscar295@gmail.com>
2022-11-29 00:03:37 +0300

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 let i = 0;
-const arrayPrograms = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const arrayPrograms = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (i = 0; i < arrayPrograms.length; i++) {
   console.log(arrayPrograms[i]);
 }
