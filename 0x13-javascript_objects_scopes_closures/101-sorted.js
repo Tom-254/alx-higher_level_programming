65fbcfb292930e636c9e7e465eddc0a0507efc95
Tish254 <tiegomseeraoscar295@gmail.com>
2022-11-30 00:13:13 +0300

    0x13. JavaScript - Objects, Scopes and Closures

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const oldDict = require('./101-data').dict;
+const newDict = {};
+for (const k in oldDict) {
+  if (oldDict[k] in newDict) {
+    newDict[oldDict[k]].push(k);
+  } else {
+    newDict[oldDict[k]] = [k];
+  }
+}
+console.log(newDict);
---
+++ 0x13-javascript_objects_scopes_closures/README.md
@@ -0,0 +1,1 @@
+# ALX Programme Project: 0x13. JavaScript - Objects, Scopes and Closures
