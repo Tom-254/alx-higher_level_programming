0c731882295c60e36197dd3450ad282712c6ac69
Tom-254 <211mseeratiego@gmail.com>
2022-12-04 20:17:51 +0000

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -2,19 +2,16 @@
 """ Script that lists all states from the database """
 import sys
 from model_state import Base, State
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    user_name = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    conection = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
-    eng = create_engine(conection.format(user_name, password, db_name),
-                        pool_pre_ping=True)
-    Session = sessionmaker(bind=eng)
-    session = Session()
-    query = session.query(State).\
-        filter(State.name.like("%a%")).order_by(State.id).all()
-    for item in query:
-        print("{}: {}".format(item.id, item.name))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+    for sql in session().query(State).filter(State.name.like('%a%')) \
+            .order_by(State.id):
+        print("{}: {}".format(sql.id, sql.name))
+    session().close()
