05cd9085578c966caca1dcf34275875dcdff9cc5
Tom-254 <211mseeratiego@gmail.com>
2022-12-04 20:13:54 +0000

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """ Script that lists all states from the database """
 import sys
-from relationship_state import State, Base
-from relationship_city import City
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -11,14 +10,12 @@ if __name__ == "__main__":
     user_name = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
-    engine = create_engine(connection.format(user_name, password, db_name))
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
+    eng = create_engine(connection.format(user_name, password, db_name),
+                        pool_pre_ping=True)
+    Session = sessionmaker(bind=eng)
     session = Session()
-
-    new_city = City(name="San Francisco")
-    new_state = State(name="California")
-    new_state.cities.append(new_city)
-    session.add_all([new_state, new_city])
+    state_to_add = State(name="Louisiana")
+    session.add(state_to_add)
     session.commit()
-    session.close()
+    obj = session.query(State).filter_by(name="Louisiana").first()
+    print(obj.id)
