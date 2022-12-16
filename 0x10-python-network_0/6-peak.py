ad29b84ee477bb414639d180e78570b323d12f40
Tish254 <tiegomseeraoscar295@gmail.com>
2022-12-16 06:06:55 +0300

    0x10. Python - Network #0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python3
-# Module that finds the peak of an unsorted list of ints
+""" function that finds a peak in a list of unsorted integers """
 
 
 def find_peak(list_of_integers):
-    """ finds the peak of an unsorted list of ints """
-    if not list_of_integers:
-        return None
-    return rfind(list_of_integers, 0, len(list_of_integers) - 1,
-                 len(list_of_integers))
-
-
-def rfind(ilist, start, end, length):
-    """ recursive function to find peak """
-    mid = start + (end - start) // 2
-    if (mid == 0 or ilist[mid - 1] <= ilist[mid]) and\
-       (mid == length - 1 or ilist[mid + 1] <= ilist[mid]):
-        return ilist[mid]
-    elif (mid > 0 and ilist[mid - 1] > ilist[mid]):
-        return rfind(ilist, start, mid - 1, length)
+    """function to find the peak"""
+    if list_of_integers:
+        list_of_integers.sort()
+        return list_of_integers[-1]
     else:
-        return rfind(ilist, mid + 1, end, length)
+        return None
