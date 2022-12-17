8c04c9b7e695349556dd612e5dd5dc60bfef4325
Tom-254 <211mseeratiego@gmail.com>
2022-12-17 05:08:16 +0000

    0x12. JavaScript - Warm up

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@ Using the requests module
 
 if __name__ == '__main__':
     import requests
-    html = requests.get('https://intranet.hbtn.io/status')
+    html = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(html.text)))
     print("\t- content: {}".format(html.text))
