e94ee34cb7d43e2be9cf3b149a3a131cdacfc3bd
Tom-254 <211mseeratiego@gmail.com>
2023-01-26 00:01:01 +0000

    0x15. JavaScript - Web jQuery

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,7 +1,7 @@
 $(function () {
     $.ajax({
         type:'GET',
-        url: 'https://www.fourtonfish.com/hellosalut/hello/,
+        url: 'https://www.fourtonfish.com/hellosalut/hello/',
         success: (function() {
             $('INPUT#btn_translate').on('click', function () {
                 $.getJSON(url + $('INPUT#language_code').val(), function (data) {
